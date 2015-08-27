a4955d6a383222167d12e6b606fcbe5bc7be9a5b
takujifunao <takuji.funao@gmail.com>
2015-08-27 15:13:33 +0900

    created auto creating question

--- create_question.jsx
+++ create_question.jsx
@@ -1,5 +1,6 @@
 
 function main(){
+	kozmin = app.textFonts.getByName("KozMinPro-Regular");
 	add_docment();
 	paste_image();
 	get_content();
@@ -39,13 +40,15 @@ function split_content(main_text){
 }
 
 function set_content(one_page){
-	// for(i=0; i<one_page.length; i++){
+	for(i=0; i<one_page.length; i++){
+		layObj = activeDocument.layers.add();
+		layObj.name = "0" + (i + 1) + "_quiz_karamon";
 		var main_content;
-		main_content = one_page[0].split("\n");
+		main_content = one_page[i].split("\n");
 		set_title(main_content[0]);
 		set_question(main_content[1]);
 		set_choices(main_content[2],main_content[3],main_content[4],main_content[5]);
-	// }
+	}
 }
 
 function set_title(title){
@@ -54,6 +57,9 @@ function set_title(title){
 	ruby.paragraphs[0].size = 70;
 	ruby.translate(1024, 1340);
 	ruby.paragraphs[0].paragraphAttributes.justification = Justification.CENTER;
+	ruby.textRange.characterAttributes.fillColor = setColor(35,24,21);
+	ruby.textRange.characterAttributes.textFont = kozmin;
+
 }
 
 function set_question(question){
@@ -61,14 +67,35 @@ function set_question(question){
 	var textRef = activeDocument.textFrames.areaText(pathRef);
 	textRef.paragraphs.add(question);
 	textRef.textRange.characterAttributes.size = 48;
+	textRef.textRange.characterAttributes.fillColor = setColor(35,24,21);
+	textRef.textRange.characterAttributes.textFont = kozmin;
 }
 
 function set_choices(one,two,three,four){
 	set_rect(700,274,600,150);
-	set_rect(700,1174,600,150);
+	set_text(645,274,600,100, one);
+
+	set_rect(700,1175,600,150);
+	set_text(645,1175,600,100, two);
+
 	set_rect(450,274,600,150);
-	set_rect(450,1174,600,150);
-	alert("one "+one);
+	set_text(395,274,600,100, three);
+
+	set_rect(450,1175,600,150);
+	set_text(395,1175,600,100, four);
+
+	set_rect(225,874,300,125);
+	set_text(185,874,300,100, "回答");
+}
+
+function set_text(x,y,w,h,choice){
+	var pathRef = activeDocument.pathItems.rectangle(x,y,w,h);
+	var textRef = activeDocument.textFrames.areaText(pathRef);
+	textRef.paragraphs.add(choice);
+	textRef.paragraphs[0].paragraphAttributes.justification = Justification.CENTER;
+	textRef.textRange.characterAttributes.size = 48;
+	textRef.textRange.characterAttributes.fillColor = setColor(35,24,21);
+	textRef.textRange.characterAttributes.textFont = kozmin;
 }
 
 function set_rect(x,y,w,h){
